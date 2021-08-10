ecdf99bf051a9f023a9815750b9b32b6b77adc3b
ariel0123 <llamas0123@hotmail.com>
2021-08-10 01:02:15 -0700

    Initial Commit

---
+++ HeartDiseaseApp/HeartDiseaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartDiseaseAppApp.swift
+//  HeartDiseaseApp
+//
+//  Created by Ariel Ortiz on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HeartDiseaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
